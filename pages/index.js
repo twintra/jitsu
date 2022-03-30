a851a86dbf4c50b7c5e65f9f1d353cd1bae4e3f9
twintra <twin.traiwin@gmail.com>
2022-03-30 11:07:20 +0700

    add bug ji

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js and apple</code>
+          <code className={styles.code}>pages/index.js and apple and hello บักจิ</code>
         </p>
 
         <div className={styles.grid}>
