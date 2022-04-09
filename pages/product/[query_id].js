77f7211d81cd20c5eafd463b0f71b4bfd8b17e47
twintra <twin.traiwin@gmail.com>
2022-04-09 15:11:19 +0700

    - add no item for failed query

--- pages/product/[query_id].js
+++ pages/product/[query_id].js
@@ -43,231 +43,239 @@ const Item = () => {
         setSelectedPayMethod(event.target.value);
     }
 
-    const RadioControl = (props) => {
-        return <Radio
-        />
-    }
+
 
 
     return (
+        bag_item === undefined ?
+            <Grid
+                container
+                style={{ minHeight: '85vh', paddingTop: "50px", paddingLeft: "10%", paddingRight: "10%" }}
+                direction="column"
 
-        <Grid
-            container
-            style={{ minHeight: '85vh', paddingTop: "50px", paddingLeft: "10%", paddingRight: "10%" }}
-            direction="column"
+                justifyContent="center"
+                alignItems="center"
 
-            justifyContent="center"
-            alignItems="center"
+            >
+                no item
+            </Grid> :
+            <Grid
+                container
+                style={{ minHeight: '85vh', paddingTop: "50px", paddingLeft: "10%", paddingRight: "10%" }}
+                direction="column"
 
-        >
-            <Box sx={{
-                maxWidth: "200px",
-                padding: "10px",
-                borderRadius: 20,
-                marginBottom: "50px",
-                border: 0.5,
-            }} >
-                <Grid container alignItems="center" justifyContent="center" >
+                justifyContent="center"
+                alignItems="center"
 
-                    <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#A9937C" }}>
+            >
+                <Box sx={{
+                    maxWidth: "200px",
+                    padding: "10px",
+                    borderRadius: 20,
+                    marginBottom: "50px",
+                    border: 0.5,
+                }} >
+                    <Grid container alignItems="center" justifyContent="center" >
 
-                        YOUR ORDER
-                    </Typography>
-                </Grid>
-            </Box>
-            <Grid container direction="row" >
-                <Grid item xs >
-                    <Grid container direction="column" alignItems="center"  >
-                        <Grid item>
+                        <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#A9937C" }}>
 
-
-                            Bag Picture
-                        </Grid>
-                        <Grid item sx={{ marginTop: "30px" }}>
-                            {bag_item.name}
-                        </Grid>
-                        <Grid item>
-                            Price : {bag_item.price} ฿
-                        </Grid>
-                        <Grid item>
-                            <Grid container direction={"row"} alignItems="center"  >
-                                <Grid item>
-                                    Amount :
-                                </Grid>
-                                <Grid item>
-                                    <Grid container direction={"column"} alignItems="center" justifyContent="center" >
-                                        <Grid item>
-                                            <IconButton onClick={clickIncrease} sx={{padding:0}}>
-                                                <ArrowDropUpIcon fontSize='large' />
-                                            </IconButton>
-
-                                        </Grid>
-                                        <Grid item>
-                                            {itemCount}
-
-                                        </Grid>
-                                        <Grid item>
-                                            <IconButton onClick={clickDecrease} sx={{padding:0}}>
-                                                <ArrowDropDownIcon fontSize='large'/>
-                                            </IconButton>
-
-                                        </Grid>
-                                    </Grid>
-
-                                </Grid>
-                                <Grid item>
-                                    Piece(s)
-                                </Grid>
-                            </Grid>
-                        </Grid>
-
-
-                    </Grid>
-                    <Link href={"/product"} passHref>
-                        <Grid item >
-
-                            <Button
-                                sx={{ color: "#000000", }}
-                            >
-
-                                <Typography >
-                                    Shop more
-
-                                </Typography>
-                            </Button>
-                        </Grid>
-                    </Link>
-
-                </Grid>
-
-                <Divider orientation='vertical' flexItem />
-                <Grid item xs>
-
-                    <Grid container direction="column" alignItems="center" >
-
-                        <Grid item sx={{ marginTop: "30px" }}>
-                            {bag_item.name} {itemCount} piece
-                        </Grid>
-                        <Grid item>
-                            Total Price : {bag_item.price * itemCount} ฿
-                        </Grid>
-
-
-
-                    </Grid>
-                    <Grid container direction={"column"} alignItems="start">
-                        <Grid item>
-                            <Grid container direction={"row"} alignItems="center" >
-                                <Box sx={{
-                                    maxWidth: "200px",
-                                    padding: "10px",
-                                    borderRadius: 20,
-
-                                    bgcolor: "#923F38",
-
-                                }} >
-
-
-                                    <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#E5E9E7" }}>
-
-                                        Payment Method
-                                    </Typography>
-
-                                </Box>
-                                <PaymentIcon fontSize='large' />
-                            </Grid>
-                        </Grid>
-                        <Grid item sx={{ paddingLeft: "20px" }}>
-                            <FormControl>
-
-                                <RadioGroup
-                                    value={selectedPayMethod}
-                                    onChange={handleRadioChange}
-                                >
-                                    <FormControlLabel
-                                        value={PayMethod.creditCard}
-                                        control={<Radio
-
-                                            icon={<CircleIcon />}
-                                            sx={{
-                                                marginRight: "10px",
-                                                padding: 0,
-                                                color: "#E5E9E7",
-                                                // backgroundColor:"green",
-                                                "&.Mui-checked": {
-                                                    color: "#000000"
-                                                },
-                                            }}
-                                        />}
-                                        label="Credit Card"
-                                    />
-                                    <FormControlLabel
-                                        value={PayMethod.bankTranfer}
-                                        control={<Radio
-
-                                            icon={<CircleIcon />}
-                                            sx={{
-                                                marginRight: "10px",
-                                                padding: 0,
-                                                color: "#E5E9E7",
-                                                // backgroundColor:"green",
-                                                "&.Mui-checked": {
-                                                    color: "#000000"
-                                                },
-                                            }}
-                                        />}
-                                        label="Bank Tranfer"
-                                    />
-                                    <FormControlLabel
-                                        value={PayMethod.cash}
-                                        control={<Radio
-
-                                            icon={<CircleIcon />}
-                                            sx={{
-                                                marginRight: "10px",
-                                                padding: 0,
-                                                color: "#E5E9E7",
-                                                // backgroundColor:"green",
-                                                "&.Mui-checked": {
-                                                    color: "#000000"
-                                                },
-                                            }}
-                                        />}
-                                        label="Cash On Delivery" />
-                                </RadioGroup>
-                            </FormControl>
-
-                        </Grid>
-
-                    </Grid>
-                </Grid>
-
-
-            </Grid>
-            <Grid item style={{ width: "100%", marginTop:"50px" }}>
-
-                <Grid container direction={"row"} alignItems="end" justifyContent="end" style={{ width: "100%" }} sx={{ width: "100%",  }} >
-                    <Box sx={{
-                        maxWidth: "200px",
-                        padding: "10px",
-                        borderRadius: 20,
-                        ":hover":{
-                            cursor:"pointer"
-                        },
-                        bgcolor: "#3A685D",
-
-                    }} >
-
-
-                        <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#E5E9E7" }}>
-
-                            Order Confirm
+                            YOUR ORDER
                         </Typography>
+                    </Grid>
+                </Box>
+                <Grid container direction="row" >
+                    <Grid item xs >
+                        <Grid container direction="column" alignItems="center"  >
+                            <Grid item>
 
-                    </Box>
+
+                                Bag Picture
+                            </Grid>
+                            <Grid item sx={{ marginTop: "30px" }}>
+                                {bag_item.name && ""}
+                            </Grid>
+                            <Grid item>
+                                Price : {bag_item.price} ฿
+                            </Grid>
+                            <Grid item>
+                                <Grid container direction={"row"} alignItems="center"  >
+                                    <Grid item>
+                                        Amount :
+                                    </Grid>
+                                    <Grid item>
+                                        <Grid container direction={"column"} alignItems="center" justifyContent="center" >
+                                            <Grid item>
+                                                <IconButton onClick={clickIncrease} sx={{ padding: 0 }}>
+                                                    <ArrowDropUpIcon fontSize='large' />
+                                                </IconButton>
+
+                                            </Grid>
+                                            <Grid item>
+                                                {itemCount}
+
+                                            </Grid>
+                                            <Grid item>
+                                                <IconButton onClick={clickDecrease} sx={{ padding: 0 }}>
+                                                    <ArrowDropDownIcon fontSize='large' />
+                                                </IconButton>
+
+                                            </Grid>
+                                        </Grid>
+
+                                    </Grid>
+                                    <Grid item>
+                                        Piece(s)
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+
+
+                        </Grid>
+                        <Link href={"/product"} passHref>
+                            <Grid item >
+
+                                <Button
+                                    sx={{ color: "#000000", }}
+                                >
+
+                                    <Typography >
+                                        Shop more
+
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Link>
+
+                    </Grid>
+
+                    <Divider orientation='vertical' flexItem />
+                    <Grid item xs>
+
+                        <Grid container direction="column" alignItems="center" >
+
+                            <Grid item sx={{ marginTop: "30px" }}>
+                                {bag_item.name && ""} {itemCount} piece
+                            </Grid>
+                            <Grid item>
+                                Total Price : {bag_item.price * itemCount} ฿
+                            </Grid>
+
+
+
+                        </Grid>
+                        <Grid container direction={"column"} alignItems="start">
+                            <Grid item>
+                                <Grid container direction={"row"} alignItems="center" >
+                                    <Box sx={{
+                                        maxWidth: "200px",
+                                        padding: "10px",
+                                        borderRadius: 20,
+
+                                        bgcolor: "#923F38",
+
+                                    }} >
+
+
+                                        <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#E5E9E7" }}>
+
+                                            Payment Method
+                                        </Typography>
+
+                                    </Box>
+                                    <PaymentIcon fontSize='large' />
+                                </Grid>
+                            </Grid>
+                            <Grid item sx={{ paddingLeft: "20px" }}>
+                                <FormControl>
+
+                                    <RadioGroup
+                                        value={selectedPayMethod}
+                                        onChange={handleRadioChange}
+                                    >
+                                        <FormControlLabel
+                                            value={PayMethod.creditCard}
+                                            control={<Radio
+
+                                                icon={<CircleIcon />}
+                                                sx={{
+                                                    marginRight: "10px",
+                                                    padding: 0,
+                                                    color: "#E5E9E7",
+                                                    // backgroundColor:"green",
+                                                    "&.Mui-checked": {
+                                                        color: "#000000"
+                                                    },
+                                                }}
+                                            />}
+                                            label="Credit Card"
+                                        />
+                                        <FormControlLabel
+                                            value={PayMethod.bankTranfer}
+                                            control={<Radio
+
+                                                icon={<CircleIcon />}
+                                                sx={{
+                                                    marginRight: "10px",
+                                                    padding: 0,
+                                                    color: "#E5E9E7",
+                                                    // backgroundColor:"green",
+                                                    "&.Mui-checked": {
+                                                        color: "#000000"
+                                                    },
+                                                }}
+                                            />}
+                                            label="Bank Tranfer"
+                                        />
+                                        <FormControlLabel
+                                            value={PayMethod.cash}
+                                            control={<Radio
+
+                                                icon={<CircleIcon />}
+                                                sx={{
+                                                    marginRight: "10px",
+                                                    padding: 0,
+                                                    color: "#E5E9E7",
+                                                    // backgroundColor:"green",
+                                                    "&.Mui-checked": {
+                                                        color: "#000000"
+                                                    },
+                                                }}
+                                            />}
+                                            label="Cash On Delivery" />
+                                    </RadioGroup>
+                                </FormControl>
+
+                            </Grid>
+
+                        </Grid>
+                    </Grid>
+
+
+                </Grid>
+                <Grid item style={{ width: "100%", marginTop: "50px" }}>
+
+                    <Grid container direction={"row"} alignItems="end" justifyContent="end" style={{ width: "100%" }} sx={{ width: "100%", }} >
+                        <Box sx={{
+                            maxWidth: "200px",
+                            padding: "10px",
+                            borderRadius: 20,
+                            ":hover": {
+                                cursor: "pointer"
+                            },
+                            bgcolor: "#3A685D",
+
+                        }} >
+
+
+                            <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "#E5E9E7" }}>
+
+                                Order Confirm
+                            </Typography>
+
+                        </Box>
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
     )
 
 }
